0056946febee59c87bc73129caf6dcbff6351ff4
Kevin <kevinolongo9@gmail.com>
2023-10-26 22:57:50 +0300

    Final commit

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,9 @@ function About({ bio, links }) {
       <h2>About Me</h2>
       {bio && bio.length > 1 ? <p>{bio}</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      <Links github={links.github} linkedin={links.linkedin} />
+      {links && links.github && (
+        <Links github={links.github} linkedin={links.linkedin} />
+      )}
     </div>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,45 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 import ProjectList from "./ProjectList";
-import user from "../data/user";
 
 function App() {
+
+  const aboutData = {
+    bio: "Some information about you goes here.",
+    links: {
+      github: "https://github.com/liza",
+      linkedin: "https://www.linkedin.com/in/liza/",
+    }
+  };
+  
+  const projects = [
+    {
+      id: 1,
+      name: "Reciplease",
+      about: "A recipe tracking app",
+      technologies: ["Rails", "Bootstrap CSS"],
+    },
+    {
+      id: 2,
+      name: "Kibbles N Bitz",
+      about: "Tender for dogs",
+      technologies: ["React", "Redux"],
+    },
+    {
+      id: 3,
+      name: "Alienwares",
+      about: "Etsy for aliens",
+      technologies: ["Vue", "CSS", "Javascript"],
+    }
+    // Add more projects here
+  ];
+
   return (
     <div>
-      <NavBar />
-      <Home username={user.name} city={user.city} color={user.color} />
-      <About bio={user.bio} links={user.links} />
-      <ProjectList projects={user.projects} />
+      <NavBar links={["home", "about", "projects"]} />
+      <Home />
+      <About {...aboutData} /> {/* Pass the aboutData object as props */}
+      <ProjectList projects={projects} />
     </div>
   );
 }
